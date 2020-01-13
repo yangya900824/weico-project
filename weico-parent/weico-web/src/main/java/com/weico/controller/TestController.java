64ddff762fb082206cd0dbf846ba6e26ebd5304a
yangya900824 <yangya900824@163.com>
2020-01-13 17:02:38 +0800

    第2次提交

--- weico-parent/weico-web/src/main/java/com/weico/controller/TestController.java
+++ weico-parent/weico-web/src/main/java/com/weico/controller/TestController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test(){
-        Test test = new Test();
-        return  test.test();
+        return  "44444";
     }
 
 }
